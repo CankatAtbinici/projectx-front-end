9aac92956a1513be8518cb70d3d08e9927a4e20f
hamzaatmaca <mentalkod09@gmail.com>
2023-01-14 23:57:03 +0300

    react-card refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Settings from "./pages/settings/Settings";
-import Register from "./pages/register/register";
+import Register from "./pages/register/Register";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import TopBar from "./components/top-bar/Top-Bar";
--- src/components/user-cards/UserCard.jsx
+++ src/components/user-cards/UserCard.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import UserCardFooter from "./UserCardFooter"
-import UserCardBody from './UserCardBody';
+import React from "react";
+import UserCardFooter from "./UserCardFooter";
+import UserCardBody from "./UserCardBody";
 
 function UserCard(props) {
-    return (
-        <div style={{
-            boxShadow: "rgb(10 0 0 / 55%) 2px 1px",
-            width: "95%",
-            
-        }}>
-            <section>
-                
-                <UserCardBody/>
-                <UserCardFooter/>
-            </section>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        boxShadow: "5px 5px 15px #e7eaed",
+        borderRadius: "8px",
+        width: "95%",
+      }}
+    >
+      <section>
+        <UserCardBody />
+        <UserCardFooter />
+      </section>
+    </div>
+  );
 }
 
 export default UserCard;
